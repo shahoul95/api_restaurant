91b2ae4a83fe76ae515b9b0cf4969b3577b6870d
shahoul95 <shahoul95@gmail.com>
2021-08-26 16:28:50 +0200

    params

--- src/app.js
+++ src/app.js
@@ -50,7 +50,14 @@ var corsOptions = {
 
 // Définition des middleware
 app.use(express.json());
-app.use(cors());
+app.use((req, res, next) => {  
+  res.setHeader('Access-Control-Allow-Origin', '*');  
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');  
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');  res.setHeader('Cross-origin-Embedder-Policy', 'require-corp');  
+  res.setHeader('Cross-origin-Opener-Policy','same-origin');  
+  if (req.method === 'OPTIONS') 
+  {   res.sendStatus(200)  } 
+  else {    next()  }});
 
 app.use(produitRouter);
 app.use(commandeRouter);
