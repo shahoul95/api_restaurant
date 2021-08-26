29d72fe8b979f414111491cc9706e4b1fbe17612
shahoul95 <shahoul95@gmail.com>
2021-08-26 13:40:52 +0200

    deploy new

--- service/Sendreceive.js
+++ service/Sendreceive.js
@@ -1,13 +1,13 @@
 const mailgun = require("mailgun-js");
-const DOMAIN = process.env.DOMAIN;
-const mg = mailgun({ apiKey: process.env.APIKEYMAILGUN, domain: DOMAIN });
+const DOMAIN = process.env.DOMAIN || "sandbox15e5d6cfc12c4302b4c4b3d213062e5b.mailgun.org";
+const mg = mailgun({ apiKey: process.env.APIKEYMAILGUN || "784e2fd011222b18c396af264b4427c8-b6d086a8-3b7c9ece", domain: DOMAIN || "sandbox15e5d6cfc12c4302b4c4b3d213062e5b.mailgun.org" });
 require('dotenv').config()
 const Sendreceive = {
 
     Sendreceives: async (req, res) => {
         const data = {
             from: req.body.from,
-            to: process.env.MAIL_USERNAME,
+            to: process.env.MAIL_USERNAME || "shahoul95@gmail.com",
             subject: req.body.subject,
             text: req.body.text
         };
